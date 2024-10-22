5f028a43322df8f15de6b7e4d9790e19f3b28a91
JCooper-42 <jamescooperpi@gmail.com>
2024-10-22 14:22:42 +0100

    Changed to only use passwords - saving time on deadline

--- App/UserHandling.py
+++ App/UserHandling.py
@@ -16,23 +16,15 @@ class Userhandling:
         return total
 
     def makeaccount(self):
-        username = input("What is your username: ")
         password = input("What is your password: ")
         salt = random.randint(0, 100) # A random value added to end to make all passwords different
         salt = str(salt) # Make a string so ord can be used
         password = password.join(salt) # join so password is with salt
         password = self.calchash(password) # calculate the modulo 26 hash
-        data = username, password, salt
+        data = password, salt
         file = open("passwords.txt", "a") # File writing
-        for i in range(0, len(data)):
-            file.write(str(data[i]))
-            if i != 2:
-                file.write(",")
-        file.write("\n")
+        file.write(str(data))
 
-
-    def search(self):
+    def search(self, password):
         file = open("passwords.txt", "r")
-        if self.username in file: # find it in the file
-            print("in file")
-            # TBC when menu is usable
+
