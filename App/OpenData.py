23cf5789d6be85b4b388b094c1dd45011352e896
JCooper-42 <jamescooperpi@gmail.com>
2024-12-05 14:49:16 +0000

    OpenHTM method started

--- App/OpenData.py
+++ App/OpenData.py
@@ -1,19 +1,27 @@
 import os, fnmatch
-import pandas as pd
+from bs4 import BeautifulSoup
 
 class fileManagment:
 
     @staticmethod
     def find(path, pattern): #Find the file on the microbit
-        result = []
+        results = []
         for root, dirs, files in os.walk(path):
             for name in files:
                 if fnmatch.fnmatch(name, pattern):
-                    result.append(os.path.join(root, name))
+                    results.append(os.path.join(root, name))
+                    result = results[0]
+                    result = str(result)
         return result
 
-    def Open(self):
+    def OpenHTM(self, pathToFile):
+        with open(pathToFile, 'r') as file:
+            soup = BeautifulSoup(file, 'html.parser')
+        tables = soup.find_all('table')
+        print(tables)
 
 
 FM = fileManagment()
-print(FM.find("D:", "MY_DATA.htm"))
+path = FM.find("D:", "MY_DATA.htm")
+print(path)
+FM.OpenHTM(path)
